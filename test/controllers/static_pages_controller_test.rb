20eb3085dca91a0fc0ccfab02679bc88c9403cb7
Oscar Mier <voscarmv@gmail.com>
2020-04-08 13:33:41 -0500

    Linter errors fixed.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get home' do
     get static_pages_home_url
-    assert_select 'title', "#{@base_title}"
+    assert_select 'title', @base_title.to_s
     assert_response :success
   end
 
